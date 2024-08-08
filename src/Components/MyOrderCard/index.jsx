b9667c4be1ed5487e3fa53a15f15eadb4966b674
fabroche <kraxusmmo@gmail.com>
2024-08-08 15:38:10 +0100

    All ready for create search module

--- src/Components/MyOrderCard/index.jsx
+++ src/Components/MyOrderCard/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './MyOrderCard.css';
 import {format} from "@formkit/tempo";
 import {PriceCurrency} from "../PriceCurrency/index.jsx";
 import {ShoppingCartIcon} from "@heroicons/react/16/solid/index.js";
@@ -8,7 +9,7 @@ function MyOrderCard({order: {date, total, products}}) {
     const totalProducts = Object.values(products).reduce((total, product) => total + product.quantity, 0)
 
     return (
-        <div className="flex-1 gap-8 flex justify-between items-center border border-black p-2 pl-4 pr-4 rounded-lg w-full h-full min-w-full">
+        <div className="my-order-card-container flex-1 gap-8 flex justify-between items-center border border-black p-2 pl-4 pr-4 rounded-lg w-full h-full min-w-full">
             <div className="flex flex-col">
             <ShoppingCartIcon
                 className="w-6 h-full cursor-pointer"
