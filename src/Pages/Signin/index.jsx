a5643c4ae4e33c0c9465c1e47efc955a6e337685
fabroche <kraxusmmo@gmail.com>
2024-08-14 16:05:44 +0100

    added conditional rendering for sign up view

--- src/Pages/Signin/index.jsx
+++ src/Pages/Signin/index.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {Layout} from "../../Components/Layout";
 import {ShopContext} from "../../Context/index.jsx";
 import {Link} from "react-router-dom";
@@ -15,9 +15,10 @@ function Signin() {
         setFilteredProductsCategories
     } = useContext(ShopContext)
 
-    return (
-        <Layout>
-            <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+    const [view, setView] = useState('login');
+
+    const renderLogIn = () => {
+        return (
             <div className="flex flex-col w-80">
                 <p>
                     <span className="font-light text-sm">Email: </span>
@@ -28,12 +29,12 @@ function Signin() {
                     <span>{account?.password?.repeat(account.password.length)}</span>
                 </p>
                 <Link
-                to="/"
-                onClick={() => setFilteredProductsCategories('')}
+                    to="/"
+                    onClick={() => setFilteredProductsCategories('')}
                 >
                     <button
-                    className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2"
-                    disabled={!signOut}
+                        className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2"
+                        disabled={account}
                     >
                         Log in
                     </button>
@@ -42,11 +43,25 @@ function Signin() {
                     <a className="font-light text-xs underline underline-offset-4" href="/">Forgot my password</a>
                 </div>
                 <button
-                className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3"
+                    className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3"
+                    onClick={() => setView('sign-up')}
                 >
-                Sign up
+                    Sign up
                 </button>
             </div>
+        )
+    }
+
+    const renderCreateUserInfo = () => {
+        return (
+            <h2 onClick={() => setView('login')}>Create your user</h2>
+        )
+    }
+
+    return (
+        <Layout>
+            <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+            {view === 'login' ? renderLogIn() : renderCreateUserInfo()}
         </Layout>
     );
 }
